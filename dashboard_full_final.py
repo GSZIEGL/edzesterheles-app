d5402d993390ad962e434756d3a421e294829890
GSZIEGL <75179091+GSZIEGL@users.noreply.github.com>
2025-05-25 19:54:32 +0200

    Delete dashboard_full_final.py

--- dashboard_full_final.py
+++
@@ -1,5 +0,0 @@
-
-# Ez a végleges dashboard_full_final.py helye lesz
-# Tartalmazni fogja az összes dashboard nézetet, benchmarkokat, top10-es listákat,
-# arányosított pókhálókat és minden szűrési lehetőséget.
-print("Végleges Streamlit app – minden dashboardfunkcióval.")
